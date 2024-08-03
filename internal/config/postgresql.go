6a25ae56c43efe1fccfa369660a9e60bd3c06080
nathakusuma <nathakusuma07@gmail.com>
2024-08-04 02:11:04 +0800

    feat(config): add PostgreSQL configuration and migration
    - Added `NewPostgresql` function to initialize PostgreSQL connection using GORM.
    - Implemented `migratePostgresqlTables` function to create custom PostgreSQL types and migrate tables.
    - Integrated user table migration in `migratePostgresqlTables`.

---
+++ internal/config/postgresql.go
@@ -0,0 +1,72 @@
+package config
+
+import (
+	"github.com/highfive-compfest/seatudy-backend/internal/user"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+)
+
+func NewPostgresql() *gorm.DB {
+	db, err := gorm.Open(postgres.Open(Env.DbDsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if err := migratePostgresqlTables(db); err != nil {
+		log.Fatalln(err)
+	}
+
+	return db
+}
+
+func migratePostgresqlTables(db *gorm.DB) error {
+	if err := db.Exec(`
+		DO $$ BEGIN
+			CREATE TYPE user_role AS ENUM (
+				'student',
+				'instructor'
+			);
+		EXCEPTION
+			WHEN duplicate_object THEN null;
+		END $$;
+	`).Error; err != nil {
+		return err
+	}
+
+	if err := db.Exec(`
+		DO $$ BEGIN
+			CREATE TYPE course_difficulty AS ENUM (
+				'beginner',
+				'intermediate',
+				'advanced',
+				'expert'
+			);
+		EXCEPTION
+			WHEN duplicate_object THEN null;
+		END $$;
+	`).Error; err != nil {
+		return err
+	}
+
+	if err := db.Exec(`
+		DO $$ BEGIN
+			CREATE TYPE post_type AS ENUM (
+				'material',
+				'assignment'
+			);
+		EXCEPTION
+			WHEN duplicate_object THEN null;
+		END $$;
+	`).Error; err != nil {
+		return err
+	}
+
+	if err := db.AutoMigrate(
+		&user.User{},
+	); err != nil {
+		return err
+	}
+
+	return nil
+}
