fb02dab9e2cba02f10c8ecd48942cf380a5dc1df
nathakusuma <nathakusuma07@gmail.com>
2024-08-04 02:13:52 +0800

    feat: add main.go with initial setup for loading environment variables, database connection, and setting up routes for user and auth

---
+++ cmd/api/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"github.com/highfive-compfest/seatudy-backend/internal/auth"
+	"github.com/highfive-compfest/seatudy-backend/internal/config"
+	"github.com/highfive-compfest/seatudy-backend/internal/middleware"
+	"github.com/highfive-compfest/seatudy-backend/internal/user"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+func main() {
+	err := godotenv.Load()
+	apiEnv := os.Getenv("ENV")
+	if err != nil && apiEnv == "" {
+		log.Println("fail to load env", err)
+	}
+	config.LoadEnv()
+
+	db := config.NewPostgresql()
+	engine := config.NewGin()
+	engine.Use(middleware.CORS())
+
+	// User
+	userRepo := user.NewRepository(db)
+	userUseCase := user.NewUseCase(userRepo)
+	user.NewRestController(engine, userUseCase)
+
+	// Auth
+	authRepo := auth.NewRepository(db)
+	authUseCase := auth.NewUseCase(authRepo, userRepo)
+	auth.NewRestController(engine, authUseCase)
+
+	if err := engine.Run(":" + config.Env.Port); err != nil {
+		log.Fatalln(err)
+	}
+}
