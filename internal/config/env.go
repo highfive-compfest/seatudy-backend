3b717273aaf8fd704f00efaa59849a06fb1cae1a
nathakusuma <nathakusuma07@gmail.com>
2024-08-05 21:14:29 +0800

    feat: add go-redis package and config

--- internal/config/env.go
+++ internal/config/env.go
@@ -7,9 +7,15 @@ import (
 )
 
 type environmentVariables struct {
-	ENV                string
-	Port               string
-	DbDsn              string
+	ENV  string
+	Port string
+
+	DbDsn string
+
+	RedisAddress  string
+	RedisPassword string
+	RedisDatabase int
+
 	JwtAccessSecret    []byte
 	JwtAccessDuration  time.Duration
 	JwtRefreshSecret   []byte
@@ -29,6 +35,13 @@ func LoadEnv() {
 	env.Port = os.Getenv("PORT")
 
 	env.DbDsn = os.Getenv("DB_DSN")
+
+	env.RedisAddress = os.Getenv("REDIS_ADDRESS")
+	env.RedisPassword = os.Getenv("REDIS_PASSWORD")
+	env.RedisDatabase, err = strconv.Atoi(os.Getenv("REDIS_DATABASE"))
+	if err != nil {
+		log.Fatal("Fail to parse REDIS_DATABASE")
+	}
 
 	env.JwtAccessSecret = []byte(os.Getenv("JWT_ACCESS_SECRET"))
 	env.JwtAccessDuration, err = time.ParseDuration(os.Getenv("JWT_ACCESS_DURATION"))
