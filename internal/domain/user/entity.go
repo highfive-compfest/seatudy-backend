8bb379fe1b1fd4b56d68f1674c934b9e8358a9b9
nathakusuma <nathakusuma07@gmail.com>
2024-08-07 01:19:25 +0800

    feat(user): change Name to maximum 50 characters

--- internal/domain/user/entity.go
+++ internal/domain/user/entity.go
@@ -17,7 +17,7 @@ type User struct {
 	ID              uuid.UUID      `json:"id" gorm:"primaryKey"`
 	Email           string         `json:"email" gorm:"type:varchar(320);unique;not null"`
 	IsEmailVerified bool           `json:"is_email_verified" gorm:"not null;default:false"`
-	Name            string         `json:"name" gorm:"type:varchar(100);not null"`
+	Name            string         `json:"name" gorm:"type:varchar(50);not null"`
 	PasswordHash    string         `json:"-" gorm:"type:char(60);not null"`
 	Role            Role           `json:"role" gorm:"type:user_role;not null"`
 	ImageURL        string         `json:"image_url" gorm:"type:text"`
