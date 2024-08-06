af4a23d29948e3db97690ed66bc20dda02cb621d
nathakusuma <nathakusuma07@gmail.com>
2024-08-07 03:11:14 +0800

    fix: remove uuid_generate_v7()
    Produces error. Better generate UUID manually.

--- internal/domain/course/entity.go
+++ internal/domain/course/entity.go
@@ -10,17 +10,17 @@ import (
 type CourseDifficulty string
 
 const (
-    Beginner     CourseDifficulty = "beginner"
-    Intermediate CourseDifficulty = "intermediate"
-    Advanced     CourseDifficulty = "advanced"
-    Expert       CourseDifficulty = "expert"
+	Beginner     CourseDifficulty = "beginner"
+	Intermediate CourseDifficulty = "intermediate"
+	Advanced     CourseDifficulty = "advanced"
+	Expert       CourseDifficulty = "expert"
 )
 
 type Course struct {
-	ID           uuid.UUID        `json:"id" gorm:"primaryKey;default:uuid_generate_v7()"`
+	ID           uuid.UUID        `json:"id" gorm:"primaryKey"`
 	Title        string           `json:"title" gorm:"type:varchar(100);not null"`
 	Description  string           `json:"description" gorm:"type:varchar(1000)"`
-	Price        float32          `json:"price" gorm:"type:numeric(11,2);not null;check:price >= 0"`
+	Price        float32          `json:"price" gorm:"type:numeric(11,2);not null"`
 	ImageURL     string           `json:"image_url" gorm:"type:text"`
 	SyllabusURL  string           `json:"syllabus_url" gorm:"type:text"`
 	InstructorID uuid.UUID        `json:"instructor_id" gorm:"not null"`
