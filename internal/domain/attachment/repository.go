e86772b3c4df6cef17d42c241dfcc05affaa8ef3
nathakusuma <nathakusuma07@gmail.com>
2024-08-11 02:54:51 +0700

    refactor: move attachment entity to internal/schema

--- internal/domain/attachment/repository.go
+++ internal/domain/attachment/repository.go
@@ -4,43 +4,44 @@ package attachment
 
 import (
 	"context"
+	"github.com/highfive-compfest/seatudy-backend/internal/schema"
 
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type Repository interface {
-    Create(ctx context.Context, att *Attachment) error
-    Update(ctx context.Context, att *Attachment) error
-	GetByID(ctx context.Context, id uuid.UUID) (*Attachment, error)
+	Create(ctx context.Context, att *schema.Attachment) error
+	Update(ctx context.Context, att *schema.Attachment) error
+	GetByID(ctx context.Context, id uuid.UUID) (*schema.Attachment, error)
 	Delete(ctx context.Context, id uuid.UUID) error
 }
 
 type repository struct {
-    db *gorm.DB
+	db *gorm.DB
 }
 
 func NewRepository(db *gorm.DB) Repository {
-    return &repository{db: db}
+	return &repository{db: db}
 }
 
-func (r *repository) Create(ctx context.Context, att *Attachment) error {
-    return r.db.WithContext(ctx).Create(att).Error
+func (r *repository) Create(ctx context.Context, att *schema.Attachment) error {
+	return r.db.WithContext(ctx).Create(att).Error
 }
 
-func (r *repository) Update(ctx context.Context, att *Attachment) error {
-    return r.db.WithContext(ctx).Save(att).Error
+func (r *repository) Update(ctx context.Context, att *schema.Attachment) error {
+	return r.db.WithContext(ctx).Save(att).Error
 }
 
-func (r *repository) GetByID(ctx context.Context, id uuid.UUID) (*Attachment, error) {
-    var att Attachment
-    result := r.db.WithContext(ctx).First(&att, "id = ?", id)
-    if result.Error != nil {
-        return nil, result.Error
-    }
-    return &att, nil
+func (r *repository) GetByID(ctx context.Context, id uuid.UUID) (*schema.Attachment, error) {
+	var att schema.Attachment
+	result := r.db.WithContext(ctx).First(&att, "id = ?", id)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return &att, nil
 }
 
 func (r *repository) Delete(ctx context.Context, id uuid.UUID) error {
-	return r.db.Delete(&Attachment{}, id).Error
+	return r.db.Delete(&schema.Attachment{}, id).Error
 }
--- internal/domain/material/entity.go
+++ internal/domain/material/entity.go
@@ -1,19 +1,19 @@
 package material
 
 import (
-	"time"
-	"github.com/highfive-compfest/seatudy-backend/internal/domain/attachment"
 	"github.com/google/uuid"
+	"github.com/highfive-compfest/seatudy-backend/internal/schema"
 	"gorm.io/gorm"
+	"time"
 )
-type Material struct {
-	ID          uuid.UUID      `json:"id" gorm:"primaryKey"`
-	CourseID    uuid.UUID      `json:"course_id" gorm:"not null"`
-	Title       string         `json:"title" gorm:"type:varchar(150);not null"`
-	Description string         `json:"description" gorm:"type:varchar(2000)"`
-	Attachments []attachment.Attachment   `json:"attachments" gorm:"foreignKey:MaterialID"`
-	CreatedAt   time.Time      `json:"created_at" gorm:"default:now();not null"`
-	UpdatedAt   time.Time      `json:"updated_at"`
-	DeletedAt   gorm.DeletedAt `json:"deleted_at" gorm:"index"`
 
+type Material struct {
+	ID          uuid.UUID           `json:"id" gorm:"primaryKey"`
+	CourseID    uuid.UUID           `json:"course_id" gorm:"not null"`
+	Title       string              `json:"title" gorm:"type:varchar(150);not null"`
+	Description string              `json:"description" gorm:"type:varchar(2000)"`
+	Attachments []schema.Attachment `json:"attachments" gorm:"foreignKey:MaterialID"`
+	CreatedAt   time.Time           `json:"created_at" gorm:"default:now();not null"`
+	UpdatedAt   time.Time           `json:"updated_at"`
+	DeletedAt   gorm.DeletedAt      `json:"deleted_at" gorm:"index"`
 }
