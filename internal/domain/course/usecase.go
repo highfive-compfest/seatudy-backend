766fd5babfbe3ec6107a7adee2a687b52f83ca62
nathakusuma <nathakusuma07@gmail.com>
2024-08-11 02:48:59 +0700

    refactor: move course entity to internal/schema

--- internal/domain/course/usecase.go
+++ internal/domain/course/usecase.go
@@ -3,6 +3,7 @@ package course
 import (
 	"context"
 	"fmt"
+	"github.com/highfive-compfest/seatudy-backend/internal/schema"
 	"log"
 	"mime/multipart"
 	"slices"
@@ -14,42 +15,41 @@ import (
 )
 
 type UseCase struct {
-    courseRepo Repository
+	courseRepo Repository
 }
 
 func NewUseCase(courseRepo Repository) *UseCase {
-    return &UseCase{courseRepo: courseRepo}
+	return &UseCase{courseRepo: courseRepo}
 }
 
-func (uc *UseCase) GetAll(ctx context.Context) ([]Course, error) {
-    return uc.courseRepo.GetAll(ctx)
+func (uc *UseCase) GetAll(ctx context.Context) ([]schema.Course, error) {
+	return uc.courseRepo.GetAll(ctx)
 }
 
-func (uc *UseCase) GetByID(ctx context.Context, id uuid.UUID) (Course, error) {
-    return uc.courseRepo.GetByID(ctx, id)
+func (uc *UseCase) GetByID(ctx context.Context, id uuid.UUID) (schema.Course, error) {
+	return uc.courseRepo.GetByID(ctx, id)
 }
 
 func (uc *UseCase) Create(ctx context.Context, req CreateCourseRequest, imageFile, syllabusFile *multipart.FileHeader, instructorID string) error {
-    var imageUrl, syllabusUrl string
-    var err error
+	var imageUrl, syllabusUrl string
+	var err error
 
 	uuidInstructorID, err := uuid.Parse(instructorID)
-    if err != nil {
-        log.Printf("Error parsing instructor ID: %v", err)
-        return ErrUnauthorizedAccess // Or any other appropriate error
-    }
+	if err != nil {
+		log.Printf("Error parsing instructor ID: %v", err)
+		return ErrUnauthorizedAccess // Or any other appropriate error
+	}
 
-    
 	id, err := uuid.NewV7()
 	if err != nil {
 		log.Println("Error generating UUID: ", err)
 		return apierror.ErrInternalServer
 	}
 
-    // Upload image if present
-    if imageFile != nil {
+	// Upload image if present
+	if imageFile != nil {
 
-        if imageFile.Size > 2*fileutil.MegaByte {
+		if imageFile.Size > 2*fileutil.MegaByte {
 			err2 := apierror.ErrFileTooLarge
 			apierror.AddPayload(&err2, map[string]string{
 				"max_size":      "2 MB",
@@ -57,14 +57,14 @@ func (uc *UseCase) Create(ctx context.Context, req CreateCourseRequest, imageFil
 			})
 			return err2
 		}
-        fileType, err := fileutil.DetectMultipartFileType(imageFile)
+		fileType, err := fileutil.DetectMultipartFileType(imageFile)
 
-        if err != nil {
+		if err != nil {
 			log.Println("Error detecting image type: ", err)
 			return apierror.ErrInternalServer
 		}
 
-        allowedTypes := fileutil.ImageContentTypes
+		allowedTypes := fileutil.ImageContentTypes
 		if !slices.Contains(allowedTypes, fileType) {
 			err2 := apierror.ErrInvalidFileType
 			apierror.AddPayload(&err2, map[string]any{
@@ -74,23 +74,22 @@ func (uc *UseCase) Create(ctx context.Context, req CreateCourseRequest, imageFil
 			return err2
 		}
 
-        imageUrl, err = config.UploadFile("course/image/" +id.String()+ "." + imageFile.Filename, imageFile)
-        if err != nil {
-            return fmt.Errorf("failed to upload image: %v", err)
-        }
-    }
-	
+		imageUrl, err = config.UploadFile("course/image/"+id.String()+"."+imageFile.Filename, imageFile)
+		if err != nil {
+			return fmt.Errorf("failed to upload image: %v", err)
+		}
+	}
 
-    // Upload syllabus if present
-    if syllabusFile != nil {
+	// Upload syllabus if present
+	if syllabusFile != nil {
 
-        fileType, err := fileutil.DetectMultipartFileType(syllabusFile)
-        
-        if err != nil {
+		fileType, err := fileutil.DetectMultipartFileType(syllabusFile)
+
+		if err != nil {
 			log.Println("Error detecting syllabus type: ", err)
 			return apierror.ErrInternalServer
 		}
-        allowedTypes := fileutil.SyllabusContentTypes
+		allowedTypes := fileutil.SyllabusContentTypes
 		if !slices.Contains(allowedTypes, fileType) {
 			err2 := apierror.ErrInvalidFileType
 			apierror.AddPayload(&err2, map[string]any{
@@ -100,125 +99,119 @@ func (uc *UseCase) Create(ctx context.Context, req CreateCourseRequest, imageFil
 			return err2
 		}
 
+		syllabusUrl, err = config.UploadFile("course/syllabus/"+id.String()+"."+syllabusFile.Filename, syllabusFile)
+		if err != nil {
+			return fmt.Errorf("failed to upload syllabus: %v", err)
+		}
+	}
 
-        syllabusUrl, err = config.UploadFile("course/syllabus/" +id.String()+ "." +  syllabusFile.Filename, syllabusFile)
-        if err != nil {
-            return fmt.Errorf("failed to upload syllabus: %v", err)
-        }
-    }
+	// Create course entity
+	course := schema.Course{
+		Title:        req.Title,
+		Description:  req.Description,
+		Price:        req.Price,
+		ImageURL:     imageUrl,
+		SyllabusURL:  syllabusUrl,
+		InstructorID: uuidInstructorID,
+		Difficulty:   req.Difficulty,
+		ID:           id,
+	}
 
-
-
-
-
-    // Create course entity
-    course := Course{
-        Title:        req.Title,
-        Description:  req.Description,
-        Price:        req.Price,
-        ImageURL:     imageUrl,
-        SyllabusURL:  syllabusUrl,
-        InstructorID: uuidInstructorID,
-        Difficulty:   req.Difficulty,
-        ID:           id,
-    }
-
-    return uc.courseRepo.Create(ctx, &course)
+	return uc.courseRepo.Create(ctx, &course)
 }
 
-func (uc *UseCase) GetByInstructorID(ctx context.Context, instructorID uuid.UUID) ([]Course, error) {
-    return uc.courseRepo.FindByInstructorID(ctx, instructorID)
+func (uc *UseCase) GetByInstructorID(ctx context.Context, instructorID uuid.UUID) ([]schema.Course, error) {
+	return uc.courseRepo.FindByInstructorID(ctx, instructorID)
 }
 
-func (uc *UseCase) Update(ctx context.Context, req UpdateCourseRequest, id uuid.UUID, imageFile, syllabusFile *multipart.FileHeader) (Course, error) {
-    // Fetch the existing course to update
-    course, err := uc.courseRepo.GetByID(ctx, id)
-    if err != nil {
-        return Course{}, fmt.Errorf("could not find course: %v", err)
-    }
+func (uc *UseCase) Update(ctx context.Context, req UpdateCourseRequest, id uuid.UUID, imageFile, syllabusFile *multipart.FileHeader) (schema.Course, error) {
+	// Fetch the existing course to update
+	course, err := uc.courseRepo.GetByID(ctx, id)
+	if err != nil {
+		return schema.Course{}, fmt.Errorf("could not find course: %v", err)
+	}
 
-    // Update fields if provided
-    if req.Title != nil {
-        course.Title = *req.Title
-    }
-    if req.Description != nil {
-        course.Description = *req.Description
-    }
-    if req.Price != nil {
-        course.Price = *req.Price
-    }
-    if req.Difficulty != nil {
-        course.Difficulty = *req.Difficulty
-    }
+	// Update fields if provided
+	if req.Title != nil {
+		course.Title = *req.Title
+	}
+	if req.Description != nil {
+		course.Description = *req.Description
+	}
+	if req.Price != nil {
+		course.Price = *req.Price
+	}
+	if req.Difficulty != nil {
+		course.Difficulty = *req.Difficulty
+	}
 
-    // Handle image update if file is provided
-    if imageFile != nil {
+	// Handle image update if file is provided
+	if imageFile != nil {
 
-        if imageFile.Size > 2*fileutil.MegaByte {
+		if imageFile.Size > 2*fileutil.MegaByte {
 			err2 := apierror.ErrFileTooLarge
 			apierror.AddPayload(&err2, map[string]string{
 				"max_size":      "2 MB",
 				"received_size": fileutil.ByteToAppropriateUnit(imageFile.Size),
 			})
-			return Course{},err2
+			return schema.Course{}, err2
 		}
-        fileType, err := fileutil.DetectMultipartFileType(imageFile)
+		fileType, err := fileutil.DetectMultipartFileType(imageFile)
 
-        if err != nil {
+		if err != nil {
 			log.Println("Error detecting image type: ", err)
-			return Course{},apierror.ErrInternalServer
+			return schema.Course{}, apierror.ErrInternalServer
 		}
 
-        allowedTypes := fileutil.ImageContentTypes
+		allowedTypes := fileutil.ImageContentTypes
 		if !slices.Contains(allowedTypes, fileType) {
 			err2 := apierror.ErrInvalidFileType
 			apierror.AddPayload(&err2, map[string]any{
 				"allowed_types": allowedTypes,
 				"received_type": fileType,
 			})
-			return Course{},err2
+			return schema.Course{}, err2
 		}
-        imageUrl, err := config.UploadFile("course/image/" +id.String()+ "." +imageFile.Filename, imageFile) // Assumes UploadFile encapsulates S3 logic
-        if err != nil {
-            return Course{}, fmt.Errorf("failed to upload image: %v", err)
-        }
-        course.ImageURL = imageUrl
-    }
+		imageUrl, err := config.UploadFile("course/image/"+id.String()+"."+imageFile.Filename, imageFile) // Assumes UploadFile encapsulates S3 logic
+		if err != nil {
+			return schema.Course{}, fmt.Errorf("failed to upload image: %v", err)
+		}
+		course.ImageURL = imageUrl
+	}
 
-    // Handle syllabus update if file is provided
-    if syllabusFile != nil {
-        fileType, err := fileutil.DetectMultipartFileType(syllabusFile)
-        
-        if err != nil {
+	// Handle syllabus update if file is provided
+	if syllabusFile != nil {
+		fileType, err := fileutil.DetectMultipartFileType(syllabusFile)
+
+		if err != nil {
 			log.Println("Error detecting syllabus type: ", err)
-			return Course{},apierror.ErrInternalServer
+			return schema.Course{}, apierror.ErrInternalServer
 		}
-        allowedTypes := fileutil.SyllabusContentTypes
+		allowedTypes := fileutil.SyllabusContentTypes
 		if !slices.Contains(allowedTypes, fileType) {
 			err2 := apierror.ErrInvalidFileType
 			apierror.AddPayload(&err2, map[string]any{
 				"allowed_types": allowedTypes,
 				"received_type": fileType,
 			})
-			return Course{},err2
+			return schema.Course{}, err2
 		}
-        syllabusUrl, err := config.UploadFile("course/syllabus/" +id.String()+ "." +syllabusFile.Filename, syllabusFile)
-        if err != nil {
-            return Course{}, fmt.Errorf("failed to upload syllabus: %v", err)
-        }
-        course.SyllabusURL = syllabusUrl
-    }
+		syllabusUrl, err := config.UploadFile("course/syllabus/"+id.String()+"."+syllabusFile.Filename, syllabusFile)
+		if err != nil {
+			return schema.Course{}, fmt.Errorf("failed to upload syllabus: %v", err)
+		}
+		course.SyllabusURL = syllabusUrl
+	}
 
-    // Update the course in the repository
-    err = uc.courseRepo.Update(ctx, &course)
-    if err != nil {
-        return Course{}, fmt.Errorf("failed to update course: %v", err)
-    }
+	// Update the course in the repository
+	err = uc.courseRepo.Update(ctx, &course)
+	if err != nil {
+		return schema.Course{}, fmt.Errorf("failed to update course: %v", err)
+	}
 
-    return course, nil
+	return course, nil
 }
-
 
 func (uc *UseCase) Delete(ctx context.Context, id uuid.UUID) error {
-    return uc.courseRepo.Delete(ctx, id)
+	return uc.courseRepo.Delete(ctx, id)
 }
