121aba4a6995f310e9e4d18cf7255c15e2b6c947
Benardo07 <117990908+Benardo07@users.noreply.github.com>
2024-08-12 01:43:50 +0700

    feat: apply pagination in repository

--- internal/domain/course/repository.go
+++ internal/domain/course/repository.go
@@ -2,19 +2,20 @@ package course
 
 import (
 	"context"
+
 	"github.com/google/uuid"
 	"github.com/highfive-compfest/seatudy-backend/internal/schema"
 	"gorm.io/gorm"
 )
 
 type Repository interface {
-	GetAll(ctx context.Context) ([]schema.Course, error)
+	GetAll(ctx context.Context, page, pageSize int) ([]schema.Course, int, error)
 	GetByID(ctx context.Context, id uuid.UUID) (schema.Course, error)
 	GetRating(ctx context.Context, courseID uuid.UUID) (float32, int64, error)
 	Create(ctx context.Context, course *schema.Course) error
 	Update(ctx context.Context, course *schema.Course) error
 	Delete(ctx context.Context, id uuid.UUID) error
-	FindByInstructorID(ctx context.Context, instructorID uuid.UUID) ([]schema.Course, error)
+	FindByInstructorID(ctx context.Context, instructorID uuid.UUID, page, pageSize int) ([]schema.Course, int, error)
 }
 
 type repository struct {
@@ -25,12 +26,26 @@ func NewRepository(db *gorm.DB) Repository {
 	return &repository{db: db}
 }
 
-func (r *repository) GetAll(ctx context.Context) ([]schema.Course, error) {
-	var courses []schema.Course
-	if err := r.db.Preload("Materials.Attachments").Find(&courses).Error; err != nil {
-		return nil, err
-	}
-	return courses, nil
+func (r *repository) GetAll(ctx context.Context, page, pageSize int) ([]schema.Course, int, error) {
+    var courses []schema.Course
+    result := r.db.Preload("Materials.Attachments").Offset((page - 1) * pageSize).Limit(pageSize).Find(&courses)
+    if result.Error != nil {
+        return nil, 0, result.Error
+    }
+    var totalRecords int64
+    r.db.Model(&schema.Course{}).Count(&totalRecords)
+    return courses, int(totalRecords), nil
+}
+
+func (r *repository) FindByInstructorID(ctx context.Context, instructorID uuid.UUID, page, pageSize int) ([]schema.Course, int, error) {
+    var courses []schema.Course
+    result := r.db.Where("instructor_id = ?", instructorID).Offset((page - 1) * pageSize).Limit(pageSize).Find(&courses)
+    if result.Error != nil {
+        return nil, 0, result.Error
+    }
+    var totalRecords int64
+    r.db.Model(&schema.Course{}).Where("instructor_id = ?", instructorID).Count(&totalRecords)
+    return courses, int(totalRecords), nil
 }
 
 func (r *repository) GetByID(ctx context.Context, id uuid.UUID) (schema.Course, error) {
@@ -61,10 +76,3 @@ func (r *repository) Delete(ctx context.Context, id uuid.UUID) error {
 	return r.db.WithContext(ctx).Delete(&schema.Course{}, id).Error
 }
 
-func (r *repository) FindByInstructorID(ctx context.Context, instructorID uuid.UUID) ([]schema.Course, error) {
-	var courses []schema.Course
-	if err := r.db.Where("instructor_id = ?", instructorID).Find(&courses).Error; err != nil {
-		return nil, err
-	}
-	return courses, nil
-}
